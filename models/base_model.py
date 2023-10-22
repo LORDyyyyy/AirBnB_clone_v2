d7f7476e1a2610fc6a9ca4d1799e2b3abf26319d
LORDyyyyy <mlord6271@gmail.com>
2023-10-22 14:51:17 +0300

    Improve engines

--- models/base_model.py
+++ models/base_model.py
@@ -24,11 +24,11 @@ class BaseModel:
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
         if kwargs:
-            kwargs.pop("__class__", None)
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                setattr(self, key, value)
+                if key != "__class__":
+                    setattr(self, key, value)
 
     def __str__(self):
         """Returns a string representation of the instance"""
