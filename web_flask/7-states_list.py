9e1dba0ed72f3e72bf30c687e7f1caf72d43edac
LORDyyyyy <mlord6271@gmail.com>
2023-10-22 17:25:27 +0300

    List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -14,10 +14,10 @@ def teardown_db(exception):
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
+    """ """
     states = sorted(storage.all(State).values(), key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
 
 
 if __name__ == '__main__':
-
     app.run(host='0.0.0.0', port=5000)
